2f845bc8ef926753b805e312ff89fbd0ada0d24c
Aishwarya <aishwaryaa1@hotmail.com>
2021-11-07 00:46:38 +0530

    jsvascript basics

---
+++ array.js
@@ -0,0 +1,12 @@
+let selectedColors = ['red' , 'blue'];
+
+//Dynamic array (size and objects)
+selectedColors[2] = 1;
+
+console.log(selectedColors);
+
+// Specify the index 
+
+console.log(selectedColors[1]);
+
+console.log(selectedColors.length);
---
+++ functions.js
@@ -0,0 +1,24 @@
+
+// PERFORMING A TASK 
+// get function parameters
+function greet(name, lastName){
+    console.log('hello ' + name + ' ' + lastName);
+}
+
+//passing the argument to the function 
+
+greet('avo' , 'mayo');
+
+
+// calculation 
+
+function square(number){
+
+    return number*number;
+}
+
+// let number = square(2);
+
+// console.log (number);
+
+console.log(square(2));
