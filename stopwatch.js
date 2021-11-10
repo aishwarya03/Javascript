78f58b4b82759217368a399a39d98977e2c5c027
Aishwarya <aishwaryaa1@hotmail.com>
2021-11-10 13:59:53 +0530

    stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,43 @@
+function Stopwatch(){
+    let startTime, endTime, duration = 0, running;
+    this.start = function(){
+        if (running){
+            throw new Error("stopwatch has already started");
+        }
+        running = true;
+
+        startTime = new Date();
+
+    };
+
+    this.stop = function(){
+        if(!running){
+            throw new Error("stopwatch is not started yet!")
+        }
+
+        running = false;
+        endTime = new Date();
+
+        const seconds = (endTime.getTime () - startTime.getTime()) /1000;
+        duration +=seconds;
+
+    };
+
+    this.reset = function(){
+        startTime = null;
+        endTime = null;
+        duration = 0;
+        running = false;
+
+    };
+
+    Object.defineProperty(this, 'duration', {
+
+        get :function(){
+            return duration;
+        }
+    });
+}
+
+
+
